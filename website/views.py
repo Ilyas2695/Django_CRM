32c7df3ad9489bee9903968fb47f40d512ec0848
Ilyas <adilkhan.ilyas1510@gmail.com>
2023-11-03 16:27:51 +0000

    added models

--- website/views.py
+++ website/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all() 
+
+
+
     # Check to see if logging in
     if request.method == "POST":
         username = request.POST['username']
@@ -18,9 +24,7 @@ def home(request):
             messages.success(request, "There was an error logging in, please try again")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
-
-    return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 
 def logout_user(request):
@@ -29,4 +33,19 @@ def logout_user(request):
     return redirect("home")
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user =authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully registered")
+            return redirect('home')
+    else:
+        # for users who have not fill out the form
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
